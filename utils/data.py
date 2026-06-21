f0df42bd2f41e568f3976e0c5e9e6645b01da8aa
Alex Frantz <alex@alexav.gg>
2026-06-21 01:04:47 -0400

    fix logo path

--- utils/data.py
+++ utils/data.py
@@ -3,4 +3,4 @@ from pathlib import Path
 
 SCRIPT_DIR = Path(__file__).parent.resolve()
 ASSET_DIR = os.path.join(SCRIPT_DIR, "../assets")
-LOGO_DIR = os.path.join(ASSET_DIR, "../logos")
+LOGO_DIR = os.path.join(ASSET_DIR, "logos")
