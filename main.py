eb654194129bfa59688640f54d8cbc58fca4a55f
Alex Frantz <alex@alexav.gg>
2026-08-02 00:52:26 -0400

    draw canvas

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ def main():
       print(canvas)
       if not canvas:
         scoreboard_matrix.canvas = scoreboard_matrix.canvas.Clear()
+        scoreboard_matrix.matrix.SwapOnVSync(scoreboard_matrix.canvas)
         scoreboard_matrix.current_slide = "fantasy"
       if canvas:
         scoreboard_matrix.canvas = scoreboard_matrix.matrix.SwapOnVSync(canvas)
