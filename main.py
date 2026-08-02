ac1467b49bea358bf6299d2c3a02167e38c38c1c
Alex Frantz <alex@alexav.gg>
2026-08-02 00:47:11 -0400

    fuck

--- main.py
+++ main.py
@@ -11,11 +11,10 @@ def main():
     print(scoreboard_matrix.current_slide)
     if scoreboard_matrix.current_slide == "score":
       canvas = score.draw_frame(scoreboard_matrix)
+      print(canvas)
       if not canvas:
         scoreboard_matrix.current_slide = "fantasy"
       if canvas:
         scoreboard_matrix.canvas = scoreboard_matrix.matrix.SwapOnVSync(canvas)
-
-    sleep(0.5)
 
 main()
